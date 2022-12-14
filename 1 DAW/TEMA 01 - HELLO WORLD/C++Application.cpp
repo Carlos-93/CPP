f42c6edb4e7c9d5f2c9738cb18e1c918d0c161f0
Carlos Araujo Galván <119673306+Carlos-93@users.noreply.github.com>
2022-12-14 10:18:08 +0100

    Add files via upload

---
+++ 1 DAW/TEMA 01 - HELLO WORLD/C++Application.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : C++Application.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!Hello World in C++ with Carlos Araujo Galván!" << endl;
+	return 0;
+}
